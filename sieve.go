ef6c186dd0aa87efaf1303e8769d2b846741bc34
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-10-12 15:39:52 -0300

    Got siege working

--- sieve.go
+++ sieve.go
@@ -1,5 +1,25 @@
 package sieve
 
+func nextPrime(n int, prime int, possiblePrimes []bool) int {
+	for i := prime; i < n; i += prime {
+		possiblePrimes[i] = true
+	}
+	for i := prime + 1; i <= n; i++ {
+		if possiblePrimes[i] == false {
+			return i
+		}
+	}
+	return n + 1
+}
+
 func sieve(n int) []int {
-	return []int{}
+	possiblePrimes := make([]bool, n+1)
+	prime := 2
+	results := []int{}
+	for prime < n {
+		results = append(results, prime)
+		prime = nextPrime(n, prime, possiblePrimes)
+	}
+
+	return results
 }
