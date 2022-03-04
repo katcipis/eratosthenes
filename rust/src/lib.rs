6250a872951571508ef345e7e941936410ae80a0
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-03-05 00:54:55 +0100

    test: add a few more tests to the sieve

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,4 +1,4 @@
-fn primes(n: u32) -> Vec<u32> {
+pub fn primes(n: u32) -> Vec<u32> {
     let mut vec: Vec<u32> = Vec::new();
     // Did lose a few hours with how to have an iterator that incrementally
     // chains more and more filters. This is the only thing that worked:
@@ -6,6 +6,13 @@ fn primes(n: u32) -> Vec<u32> {
     // - https://users.rust-lang.org/t/multiple-staggered-filters-on-an-iterator/60959/4
     //
     // The learning curve for this stuff is steep as fuck x_x.
+    // Tried to model primes also as an iterator/stream, but the borrow
+    // checker defeated me at my current novice level x_x.
+    // Code very similar to this, but using a struct with the boxed type,
+    // simply didn't work, there was always some different lifetime/reference
+    // issue that I was unable to solve properly x_x.
+    // In the end it could have worked if it was easy to declare a trait that is
+    // an Iterator + Clone, but it is not easy AFAIK.
     let mut iter: Box<dyn Iterator<Item = u32> + '_> = Box::new(NaturalNumbers::new(2));
 
     for _ in 0..n {
@@ -31,7 +38,6 @@ impl Iterator for NaturalNumbers {
     type Item = u32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        // TODO: handle overflow, won't panic in non-debug mode
         let val = self.cur;
         self.cur += 1;
         Some(val)
@@ -60,8 +66,18 @@ mod tests {
 
     #[test]
     fn primes_generation() {
+        let p = primes(0);
+
+        assert_eq!(p.len(), 0);
+
+        let p = primes(1);
+
+        assert_eq!(p.len(), 1);
+        assert_eq!(p[0], 2);
+
         let p = primes(10);
 
+        assert_eq!(p.len(), 10);
         assert_eq!(p[0], 2);
         assert_eq!(p[1], 3);
         assert_eq!(p[2], 5);
